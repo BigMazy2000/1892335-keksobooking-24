46afe8efd1497e0e56f4b300ecd800165897761a
BigMazy2000 <maximazaytsev@gmail.com>
2021-11-14 23:03:49 +1100

    выполненные задания

---
+++ js/api.js
@@ -0,0 +1,37 @@
+import {
+  showAlert
+} from './utils/alert-messages.js';
+import {
+  showSubmitOk
+} from './utils/alert-messages.js';
+const getData = (onSuccess) => {
+  fetch('https://24.javascript.pages.academy/keksobooking/data')
+    .then((response) => response.json())
+    .then((domain) => {
+      onSuccess(domain);
+    })
+    .catch(() => {
+      showAlert('Не удалось получить данные. Попробуйте ещё раз');
+    });
+};
+const sendData = (onSuccess, body) => {
+  fetch('https://24.javascript.pages.academy/keksobooking', {
+    method: 'POST',
+    body,
+  } )
+    .then((response) => {
+      if (response.ok) {
+        onSuccess();
+        showSubmitOk();
+      } else {
+        showAlert('Не удалось отправить форму. Попробуйте ещё раз');
+      }
+    })
+    .catch(() => {
+      showAlert('Не удалось отправить форму. Попробуйте ещё раз');
+    });
+};
+export {
+  getData,
+  sendData
+};
